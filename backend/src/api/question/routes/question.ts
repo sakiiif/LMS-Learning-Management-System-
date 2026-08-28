00a12e76c19680e32f6d0c5c673670bd5c82d333
= <=>
2026-08-28 20:57:43 +0600

    Quiz and Questions Ownership Policy implemented

--- backend/src/api/question/routes/question.ts
+++ backend/src/api/question/routes/question.ts
@@ -4,4 +4,22 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreRouter('api::question.question');
+export default factories.createCoreRouter('api::question.question', {
+  config: {
+    create: {
+      policies: [
+        { name: 'global::is-quiz-course-instructor', config: { contentType: 'api::question.question' } },
+      ],
+    },
+    update: {
+      policies: [
+        { name: 'global::is-quiz-course-instructor', config: { contentType: 'api::question.question' } },
+      ],
+    },
+    delete: {
+      policies: [
+        { name: 'global::is-quiz-course-instructor', config: { contentType: 'api::question.question' } },
+      ],
+    },
+  },
+});
--- backend/src/api/quiz/routes/quiz.ts
+++ backend/src/api/quiz/routes/quiz.ts
@@ -4,4 +4,22 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreRouter('api::quiz.quiz');
+export default factories.createCoreRouter('api::quiz.quiz', {
+  config: {
+    create: {
+      policies: [
+        { name: 'global::is-course-instructor', config: { contentType: 'api::quiz.quiz' } },
+      ],
+    },
+    update: {
+      policies: [
+        { name: 'global::is-course-instructor', config: { contentType: 'api::quiz.quiz' } },
+      ],
+    },
+    delete: {
+      policies: [
+        { name: 'global::is-course-instructor', config: { contentType: 'api::quiz.quiz' } },
+      ],
+    },
+  },
+});
